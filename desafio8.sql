29ffbe5ea8fe96cef74e1fb6235126d9c7685dcd
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-08-24 21:02:42 -0300

    feat: requisito 8, filtra nome de artista específico e o nome de seus albuns.

---
+++ desafio8.sql
@@ -0,0 +1,7 @@
+SELECT
+art.nome_artista AS artista,
+alb.nome_album AS album
+FROM SpotifyClone.artistas AS art
+INNER JOIN SpotifyClone.albuns AS alb
+ON art.artista_id = alb.artista_id
+WHERE art.nome_artista = 'Elis Regina';
